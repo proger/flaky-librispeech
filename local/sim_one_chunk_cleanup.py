89dcba1b201bbbd3df341568ced16d2e45e316ea
Volodymyr Kyrylov <vol@wilab.org.ua>
2023-07-27 18:54:55 +0300

    simulate cleanup of only one chunk

--- local/sim_one_chunk_cleanup.py
+++ local/sim_one_chunk_cleanup.py
@@ -2,11 +2,11 @@
 
 import argparse
 import pandas as pd
+import itertools
 
-parser = argparse.ArgumentParser(description='simulate uniform cleanups for active learning')
+parser = argparse.ArgumentParser(description='simulate one random chunk cleanup for active learning')
 parser.add_argument('--seed', type=int, default=42)
-parser.add_argument('--topk', type=int, help='generate this many chunks of mixtures')
-args = parser.parse_args()
+args = parser.parse_args()  
 
 ref = pd.read_csv('data/corrupted-librispeech/train-clean-100.ref.txt', sep='\t')
 ep45 = pd.read_csv('data/corrupted-librispeech/train-clean-100.ep45.txt', sep='\t')
@@ -18,11 +18,10 @@ both.columns = ['filename', 'dirty', 'filename1', 'clean']
 chunk_size = 2196
 
 chunks = [both[i:i+chunk_size] for i in range(0,len(both),chunk_size)]
-topk = args.topk if args.topk else len(chunks)
 
-for i in range(0,len(chunks)+1)[::-1][:topk]:
-    clean = pd.concat(chunks[:i]) if chunks[:i] else both.sample(0)
-    dirty = pd.concat(chunks[i:]) if chunks[i:] else both.sample(0)
+for i in range(0,len(chunks)):
+    clean = chunks[i]
+    dirty = pd.concat(chunks[:i] + chunks[i+1:])
 
     clean = clean[['filename', 'clean']]
     clean.columns = ['filename', 'text']
@@ -33,8 +32,8 @@ for i in range(0,len(chunks)+1)[::-1][:topk]:
     dataset = pd.concat([clean, dirty])
     dataset = dataset.sample(frac=1, replace=False, random_state=i)
 
-    filename = f'data/corrupted-librispeech/train-clean-100.seed{args.seed}.dirty{len(dirty):05d}.txt'
+    filename = f'data/corrupted-librispeech/train-clean-100.seed{args.seed}.dirty{len(dirty):05d}.clean{i:02d}.txt'
     print(filename, i, len(dirty), len(clean), len(dataset))
     dataset.to_csv(filename, sep='\t', index=False, header=False)
     
-print([len(chunk) for chunk in chunks], len(chunks))
+print([len(chunk) for chunk in chunks], len(chunks), sum([len(chunk) for chunk in chunks]))
